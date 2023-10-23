5b0c195ecc7023437379794d8c7a5e8cb4aee70f
MylAnt95 <anton.myllykoski95@gmail.com>
2023-10-23 16:15:53 +0200

    added css and a loop that displays all the books and their data

--- books.php
+++ books.php
@@ -1,5 +1,94 @@
-<?php 
+<?php
 
-$Books = [
-    
-]
+$books = [
+    'The Silver Serpent' => [
+        'id' => '12345',
+        'author' => 'Frank Novelist',
+        'color' => 'Silver',
+        'genre' => 'Fantasy'
+    ],
+    'Crimson Moonrise' => [
+        'id' => '54321',
+        'author' => 'Grace Writer',
+        'color' => 'Red',
+        'genre' => 'Mystery'
+    ],
+    'Emerald Echoes' => [
+        'id' => '67890',
+        'author' => 'Henry Wordsmith',
+        'color' => 'Green',
+        'genre' => 'Romance'
+    ],
+    'The Golden Key' => [
+        'id' => '89797',
+        'author' => 'Isabel Storyteller',
+        'color' => 'Yellow',
+        'genre' => 'Adventure'
+    ],
+    'Sapphire Shadows' => [
+        'id' => '23556',
+        'author' => 'Jack Author',
+        'color' => 'Blue',
+        'genre' => 'Thriller'
+    ],
+    'Scarlet Secrets' => [
+        'id' => '56341',
+        'author' => 'Katherine Novelist',
+        'color' => 'Red',
+        'genre' => 'Mystery'
+    ],
+    'The Bronze Blade' => [
+        'id' => '32545',
+        'author' => 'Liam Writer',
+        'color' => 'Bronze',
+        'genre' => 'Historical Fiction'
+    ],
+    'Violet Vendetta' => [
+        'id' => '93275',
+        'author' => 'Mia Wordsmith',
+        'color' => 'Purple',
+        'genre' => 'Crime'
+    ],
+    'The Enchanted Forest' => [
+        'id' => '23564',
+        'author' => 'Nathan Storyteller',
+        'color' => 'Green',
+        'genre' => 'Fantasy'
+    ],
+    'Ruby Redemption' => [
+        'id' => '12456',
+        'author' => 'Olivia Author',
+        'color' => 'Red',
+        'genre' => 'Romance'
+    ],
+    'The Midnight Murders' => [
+        'id' => '32675',
+        'author' => 'Patrick Novelist',
+        'color' => 'Black',
+        'genre' => 'Mystery'
+    ],
+    'Aqua Odyssey' => [
+        'id' => '65488',
+        'author' => 'Quincy Writer',
+        'color' => 'Aqua',
+        'genre' => 'Adventure'
+    ],
+    'Copper Chronicles' => [
+        'id' => '23298',
+        'author' => 'Rachel Wordsmith',
+        'color' => 'Copper',
+        'genre' => 'Fantasy'
+    ],
+    'Topaz Treasures' => [
+        'id' => '53489',
+        'author' => 'Samuel Storyteller',
+        'color' => 'Orange',
+        'genre' => 'Adventure'
+    ],
+    'Onyx Obsession' => [
+        'id' => '89845',
+        'author' => 'Tara Autohew',
+        'color' => 'Black',
+        'genre' => 'Mystery'
+    ]
+];
--- index.php
+++ index.php
@@ -1,15 +1,52 @@
 <?php
 require __DIR__ . '/functions.php';
-?>
+require __DIR__ . '/books.php';
 
+/* $titles = [];
+foreach ($books as $book => $data) {
+    $titles[] = $data['title'];
+}
+if (isset($_GET['direction'])) {
+    if ($_GET['direction'] === 'asc') {
+        $_GET['direction'] = sort($titles);
+    } elseif ($_GET['direction'] === 'desc') {
+        $_GET['direction'] = rsort($titles);
+    }
+} */
+
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"><link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css">
     <title>Bookshelf</title>
 </head>
+<nav>
+</nav>
+
 <body>
-    
+    <div class="dropdown">
+        <button class="dropbtn">Sort by</button>
+        <div class="dropdown-content">
+            <a href="?direction=asc">ASC</a>
+            <a href="?direction=desc">DESC</a>
+        </div>
+    </div>
+    <div class="shelf">
+        <?php foreach ($books as $book => $bookData) : ?>
+            <div class="book">
+                <h4 class="book-title"><?= $book; ?></h4>
+                <p class="book-id"><?= $bookData['id']; ?></p>
+                <p class="book-author"><?= $bookData['author']; ?></p>
+                <p class="book-genre"><?= $bookData['genre']; ?></p>
+            </div>
+        <?php endforeach; ?>
+    </div>
 </body>
+
 </html>
